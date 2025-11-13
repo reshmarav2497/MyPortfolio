4b4e6325156abbeb3974dffe112159b398ddfe26
reshma <reshmaraveendra2497@gmail.com>
2025-11-14 02:15:07 +0530

    commit changes

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -35,7 +35,7 @@ export const AboutMeSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/16TkKmxaocqNoFW9P9OkPmkeg6WtL9qqD/view?usp=sharing"
+                href={import.meta.env.VITE_RESUME_URL}
                 target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
